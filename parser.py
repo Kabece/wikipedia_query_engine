52224e79822253f7778aa9e87c75ede1a1b0d1d7
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-10-05 15:33:40 +0200

    Parsing whole file and leaving text only

--- parser.py
+++ parser.py
@@ -1,31 +1,24 @@
 from lxml import etree
-from copy import deepcopy
+from bz2 import BZ2File
+import re
 
 def fast_iter(context, func):
         for event, elem in context:
-            func(elem)
+            if elem is not None:
+                func(elem)
             elem.clear()
             for ancestor in elem.xpath('ancestor-or-self::*'):
                 while ancestor.getprevious() is not None:
                     del ancestor.getparent()[0]
         del context
 
-def serialize(elem):
-    r = etree.Element('SimplerRecord')
-    t = etree.SubElement(r, 'title')
-    t.text = next(elem.iterchildren(tag='title')).text
+def textNode(elem):
+    t = next(next(elem.iterchildren(tag='{http://www.mediawiki.org/xml/export-0.10/}revision')).iterchildren(tag='{http://www.mediawiki.org/xml/export-0.10/}text')).text
+    t = re.sub("<[^<]+>", "", t)
+    return t
 
-    # for c in elem.iterchildren(tag='Copyright'):
-    #     r.append(deepcopy(c))
-    return r
-
-
-googleDataFile = "E:/Downloads/GoogleData/google.xml"
 wikipediaDataFile = "E:/Downloads/WikiData/wiki.xml"
 
-
-context = etree.iterparse(wikipediaDataFile, events=('end',), tag='page')
-out = open('E:/Downloads/WikiData/workfile.xml', 'wb', 16777216)
-
-fast_iter(context, lambda elem: out.write(etree.tostring(serialize(elem), encoding='utf-8')))
-out.close()
+with BZ2File(wikipediaDataFile) as wiki, open('E:/Downloads/WikiData/workfile.xml', 'w', encoding='utf-8') as output_file:
+    context = etree.iterparse(wikipediaDataFile, events=('end',), tag='{http://www.mediawiki.org/xml/export-0.10/}page')
+    fast_iter(context, lambda elem: output_file.write(textNode(elem)))
