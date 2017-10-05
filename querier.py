6359247f8b9375e60ada2c6347af263a779aa263
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-10-05 16:51:58 +0200

    Correct query finding

--- querier.py
+++ querier.py
@@ -1,10 +1,25 @@
-from lxml import etree
-
-def find():
-    pass
+import re
 
 filePath = "E:/Downloads/WikiData/workfile.xml"
 
-with open(filePath, "w", encoding='utf-8') as file:
+def makeRegexp(query):
+    query = re.sub("\"", "", query)
+    query = re.sub("\[", ".{", query)
+    query = re.sub("\]", "}", query)
+    pattern = re.compile(query)
+    return pattern
+
+with open(filePath, "r", encoding='utf-8') as file:
     query = input("Enter query to search for: ")
-    print("Here's your query: ", query)
+    makeRegexp(query)
+    for i, line in enumerate(file):
+        pattern = makeRegexp(query)
+        for match in re.findall(pattern, line):
+            print("Found on line %s: %s" % (i + 1, match))
+
+
+# The '*', '+', and '?' qualifiers are all greedy; they match as much text as possible.
+    # Sometimes this behaviour isn’t desired; if the RE <.*> is matched against <a> b <c>, it
+    # will match the entire string, and not just <a>. Adding ? after the qualifier makes it perform
+    # the match in non-greedy or minimal fashion; as few characters as possible will be matched.
+    # Using the RE <.*?> will match only <a>.
